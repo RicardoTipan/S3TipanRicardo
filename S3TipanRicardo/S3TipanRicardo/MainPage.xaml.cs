0aca7d287c03574f993730a459af6f743b5dc2d5
Ricardo Tipàn Toapanta <javi_asm@hotmail.com>
2020-10-30 10:46:37 -0500

    Navegacio de Layouts y Login

---
+++ S3TipanRicardo/S3TipanRicardo/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace S3TipanRicardo
+{
+    public partial class MainPage : ContentPage
+    {
+
+        string errorMessage = null;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void limpiar()
+        {
+            txtUser.Text = string.Empty;
+            txtPass.Text = string.Empty;
+        }
+
+        private async void bntventana1_Clicked(object sender,EventArgs e)
+        {
+            //abrir vista dos
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+            if (txtUser.Text=="estudiante2020" && txtPass.Text=="uisrael2020" )
+            {
+                await DisplayAlert("Acceso Correcto", errorMessage, "OK");
+                await Navigation.PushAsync(new VistaDos(user, pass));
+
+            }
+            else
+            {
+                await DisplayAlert("Usuario o Contraseña Incorrecta", errorMessage, "OK");
+                await Navigation.PushAsync(new MainPage());
+                limpiar();
+              
+            }
+
+            
+        }
+
+    }
+
+}
---
+++ S3TipanRicardo/S3TipanRicardo/VistaDos.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S3TipanRicardo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VistaDos : ContentPage
+    {
+        public VistaDos(string user, string pass)
+        {
+            InitializeComponent();
+            string usuario = lblUser.Text;
+            lblUser.Text = usuario + user;
+        }
+    }
+}
